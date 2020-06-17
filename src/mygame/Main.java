8d6e84845d4055b6276c6b5af980de8699830701
Joshua Sigona <sigonasr2@gmail.com>
2020-06-17 12:25:13 +0900

    Added level assets and water effects

--- src/mygame/Main.java
+++ src/mygame/Main.java
@@ -3,9 +3,18 @@ package mygame;
 import com.jme3.app.SimpleApplication;
 import com.jme3.material.Material;
 import com.jme3.math.ColorRGBA;
+import com.jme3.math.Vector3f;
+import com.jme3.post.FilterPostProcessor;
 import com.jme3.renderer.RenderManager;
+import com.jme3.renderer.queue.RenderQueue;
+import com.jme3.renderer.queue.RenderQueue.Bucket;
 import com.jme3.scene.Geometry;
+import com.jme3.scene.Node;
+import com.jme3.scene.Spatial;
 import com.jme3.scene.shape.Box;
+import com.jme3.util.SkyFactory;
+import com.jme3.water.SimpleWaterProcessor;
+import com.jme3.water.WaterFilter;
 
 /**
  * This is the Main Class of your Game. You should only do initialization here.
@@ -21,14 +30,8 @@ public class Main extends SimpleApplication {
 
     @Override
     public void simpleInitApp() {
-        Box b = new Box(1, 1, 1);
-        Geometry geom = new Geometry("Box", b);
-
-        Material mat = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
-        mat.setColor("Color", ColorRGBA.Blue);
-        geom.setMaterial(mat);
-
-        rootNode.attachChild(geom);
+        flyCam.setMoveSpeed(50);
+        CreateLevel();
     }
 
     @Override
@@ -39,5 +42,33 @@ public class Main extends SimpleApplication {
     @Override
     public void simpleRender(RenderManager rm) {
         //TODO: add render code
+    }
+
+    private void CreateLevel() {
+        Node reflectedScene = new Node("Reflected Scene");
+        rootNode.attachChild(reflectedScene);
+        Spatial TestLevel = assetManager.loadModel("Scenes/TestLevel.j3o");
+        Node world = (Node)TestLevel;
+        System.out.println(world.getChildren());
+        reflectedScene.attachChild(TestLevel);
+        reflectedScene.attachChild(SkyFactory.createSky(assetManager,"Textures/Sky/Bright/BrightSky.dds",false));
+        
+        /*SimpleWaterProcessor waterProcessor = new SimpleWaterProcessor(assetManager);
+        waterProcessor.setReflectionScene(reflectedScene);
+        waterProcessor.setWaterColor(new ColorRGBA(0.0f,0.0f,0.0f,1.0f));
+        waterProcessor.setWaterDepth(2);
+        waterProcessor.setWaterTransparency(0.9f);
+        waterProcessor.setWaveSpeed(0.02f);
+        waterProcessor.setDistortionScale(0.3f);
+        waterProcessor.setDistortionMix(0.6f);
+        waterProcessor.
+        viewPort.addProcessor(waterProcessor);
+        world.getChild("WaterNode").setMaterial(waterProcessor.getMaterial());*/
+        FilterPostProcessor fpp = new FilterPostProcessor(assetManager);
+        viewPort.addProcessor(fpp);
+        Vector3f lightDir = new Vector3f(-2.9f,-1.2f,-5.8f);
+        WaterFilter water = new WaterFilter(reflectedScene, lightDir);
+        fpp.addFilter(water);
+        //world.getChild("WaterNode").setQueueBucket(Bucket.Transparent);
     }
 }
